59c5c3264c1e580b9fb16379d16cb8baac793ba0
MincoMK <mail@drchi.co.kr>
2024-01-09 22:02:39 +0900

    .

--- shaders/test_f.glsl
+++ shaders/test_f.glsl
@@ -30,28 +30,7 @@ void main() {
         float spec = pow(max(dot(viewDir, reflectDir), 0.0f), 32);
         vec3 specular = specularStrength * spec * lightColors[i];
 
-        // shadow
-        vec3 lightPos = lightPositions[i];
-        vec3 fragPos = pos;
-        vec3 lightDir2 = normalize(lightPos - fragPos);
-        float shadow = 0.0f;
-        vec3 fragPos2 = fragPos + normal * 0.001f;
-        float bias = 0.001f;
-        float maxDistance = 100.0f;
-        int shadowSamples = 10;
-        float sampleStep = maxDistance / float(shadowSamples);
-        for (int i = 0; i < shadowSamples; i++) {
-            float distance = length(lightPos - fragPos2);
-            if (distance < length(lightPos - fragPos)) {
-                shadow += 1.0f;
-            }
-            fragPos2 += lightDir2 * sampleStep;
-        }
-        shadow /= float(shadowSamples);
-        shadow = clamp(shadow, 0.0f, 1.0f);
-
-        // result
-        result += (ambient + (1.0f - shadow) * (diffuse + specular)) * objectColor;
+        result += (ambient + diffuse + specular) * objectColor * 3;
     }
 
     FragColor = vec4(result, 1.0f);
